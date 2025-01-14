b5fafb788550ba4a74ab519964aa989e265d58c2
Thiago Lopes do Nascimento <tldnascimento@service0.npad.internal>
2025-01-14 17:14:50 -0300

    dados do NPAD Intel-128

---
+++ pascalanalyzer_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=intel-128
+#SBATCH --job-name=PaScal_job
+#SBATCH --output=PaScal_job%j.out
+#SBATCH --error=PaScal_job%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-5:10
+
+pascalanalyzer -t aut -c 1,2,4,8,16,32 -i 7,13,25,50,100,200,400,800 -o main_read_txt.json ./main_read_txt
+
