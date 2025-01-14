acce16126d35d860bcb419c31f6f184d4b22fc00
Thiago Lopes do Nascimento <tldnascimento@service0.npad.internal>
2025-01-14 17:46:18 -0300

    Dados com 5 repetições

--- pascalanalyzer_job.sh
+++ pascalanalyzer_job.sh
@@ -7,5 +7,5 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-5:10
 
-pascalanalyzer -t aut -c 1,2,4,8,16,32 -i 7,13,25,50,100,200,400,800 -o main_read_txt.json ./main_read_txt
+pascalanalyzer -t aut -c 1,2,4,8,16,32,64,128 -i 7,13,25,50,100,200,400,800 -r 5 -o main_read_txt.json ./main_read_txt
 
